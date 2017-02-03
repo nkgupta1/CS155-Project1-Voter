0278a77d3ddf59b407c227fb81218811583b84db
Nikhil Gupta <nikhil97gupta@gmail.com>
2017-02-03 14:45:04 -0800

    data importing methods

--- src/hashe_tests.py
+++ src/hashe_tests.py
@@ -4,20 +4,23 @@ import numpy as np
 import time
 import csv
 
-if __name__=='__main__':
-    start = time.time()
-    train_data = np.genfromtxt('../data/train_2008.csv', delimiter=',')
-    end = time.time()
-
-    time1 = end-start
-    print(time1)
-
-    start2 = time.time()
-    with open('../data/train_2008.csv','r') as dest_f:
+def importTrain(filename):
+    with open(filename,'r') as dest_f:
         data_iter = csv.reader(dest_f, delimiter=',')
         data = [data for data in data_iter]
     data_array = np.asarray(data)
-    end2 = time.time()
+    # remove first row, first column and last column
+    X_train = data_array[1:,1:-1]
+    # remove first row, keep last column
+    Y_train = data_array[1:,-1]
+    return X_train, Y_train
 
-    time2 = end2-start2
-    print(time2)
+def importTest(filename):
+    with open(filename,'r') as dest_f:
+        data_iter = csv.reader(dest_f, delimiter=',')
+        data = [data for data in data_iter]
+    data_array = np.asarray(data)
+    # remove first row, first column
+    X_train = data_array[1:,1:]
+    return X_train
+    
