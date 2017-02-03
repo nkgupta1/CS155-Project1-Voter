86ab4caa47ce6f3b33c95103d50899f7a4081d21
vanand-dev <vanand@caltech.edu>
2017-02-03 14:59:34 -0800

    float return

--- src/common/__init__.py
+++ src/common/__init__.py
@@ -7,30 +7,28 @@ in every file.
 import numpy as np
 import csv
 
-def importTrain(filename):
+def import_train(filename):
     """
     Imports training data and parses it
     """
     with open(filename,'r') as dest_f:
         data_iter = csv.reader(dest_f, delimiter=',')
         data = [data for data in data_iter]
-    data_array = np.asarray(data)
     # remove first row (column labels), first column (id) and last column
-    # (label)
-    X_train = data_array[1:,1:-1]
-    # remove first row (column labels), keep last column (label)
-    Y_train = data_array[1:,-1]
+    data_array = np.asarray(data)[1:, 1:]
+    # convert to 32-bit float
+    data_array = data_array.astype(np.float32)
+    X_train, Y_train = data_array[:, :-1], data_array[:, -1]
     return X_train, Y_train
 
-def importTest(filename):
+def import_test(filename):
     """
     Imports test data and parses it
     """
     with open(filename,'r') as dest_f:
         data_iter = csv.reader(dest_f, delimiter=',')
         data = [data for data in data_iter]
-    data_array = np.asarray(data)
-    # remove first row (column labels), first column (id)
-    X_train = data_array[1:,1:]
+    data_array = np.asarray(data)[1:, 1:]
+    X_train = data_array.astype(np.float32)
     return X_train
     
