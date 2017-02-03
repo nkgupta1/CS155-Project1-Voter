e7a53ac4e183195f240bb9bfdda7eb2177380a29
Nikhil Gupta <nikhil97gupta@gmail.com>
2017-02-03 14:50:49 -0800

    import data methods

--- src/common/__init__.py
+++ src/common/__init__.py
@@ -7,14 +7,30 @@ in every file.
 import numpy as np
 import csv
 
-def import_data(filename):
+def importTrain(filename):
     """
-    Imports data from provided csv file, returns as
-    numpy array.
+    Imports training data and parses it
     """
-
     with open(filename,'r') as dest_f:
         data_iter = csv.reader(dest_f, delimiter=',')
         data = [data for data in data_iter]
-    data = np.asarray(data)
-    data = data.astype(np.float32)
+    data_array = np.asarray(data)
+    # remove first row (column labels), first column (id) and last column
+    # (label)
+    X_train = data_array[1:,1:-1]
+    # remove first row (column labels), keep last column (label)
+    Y_train = data_array[1:,-1]
+    return X_train, Y_train
+
+def importTest(filename):
+    """
+    Imports test data and parses it
+    """
+    with open(filename,'r') as dest_f:
+        data_iter = csv.reader(dest_f, delimiter=',')
+        data = [data for data in data_iter]
+    data_array = np.asarray(data)
+    # remove first row (column labels), first column (id)
+    X_train = data_array[1:,1:]
+    return X_train
+    
