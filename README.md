1c25dec192946fb352c46efa572f69c734293c65
Nikhil Gupta <nikhil97gupta@gmail.com>
2017-02-03 15:27:54 -0800

    TODO list

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 
 This is a project for CS155 Machine Learning and Data Mining at Caltech.
 
+TODO:
+    Convert some columns to classes as opposed to integers
--- src/nkgupta-test1.py
+++ src/nkgupta-test1.py
@@ -6,10 +6,15 @@ A naive test on the 2008 data.
 from common import import_train
 import numpy as np 
 import tensorflow as tf 
-import keras
-from keras.models import Sequential
-from keras.layers.core import Dense, Activation, Flatten, Dropout
+# import keras
+# from keras.models import Sequential
+# from keras.layers.core import Dense, Activation, Flatten, Dropout
 
 X_train_2008, Y_train_2008 = import_train('../data/train_2008.csv')
 
+# print(X_train_2008.shape)
+# print(Y_train_2008[0])
 
+# model = Sequential()
+
+# model.add(Dense(750))
