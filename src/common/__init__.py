18edd59aa34389c4576666e8daef0ad4728ef4e4
Nikhil Gupta <nikhil97gupta@gmail.com>
2017-02-03 15:13:55 -0800

    updated data importing

--- src/common/__init__.py
+++ src/common/__init__.py
@@ -15,12 +15,18 @@ def import_train(filename):
     with open(filename,'r') as dest_f:
         data_iter = csv.reader(dest_f, delimiter=',')
         data = [data for data in data_iter]
-    data_array = np.asarray(data)
-    # remove first row (column labels), first column (id) and last column (label)
+
+    # remove first row (column labels), first column (id)
     data_array = np.asarray(data)[1:, 1:]
-    # convert to 32-bit float
-    data_array = data_array.astype(np.float32)
-    X_train, Y_train = data_array[:, :-1], data_array[:, -1]
+
+    # convert to 32-bit int
+    data_array = data_array.astype(np.int32)
+     
+    #  remove last column (label)
+    X_train = data_array[:, :-1]
+    #  keep last column (label) and make into one hot vector
+    Y_train = keras.utils.np_utils.to_categorical(data_array[:, -1])
+
     return X_train, Y_train
 
 def import_test(filename):
--- src/nkgupta-test1.py
+++ src/nkgupta-test1.py
@@ -3,13 +3,13 @@
 A naive test on the 2008 data.
 """
 
-from common import importTrain
+from common import import_train
 import numpy as np 
 import tensorflow as tf 
 import keras
 from keras.models import Sequential
 from keras.layers.core import Dense, Activation, Flatten, Dropout
 
-X_train_2008, Y_train_2008 = importTrain('../data/train_2008.csv')
+X_train_2008, Y_train_2008 = import_train('../data/train_2008.csv')
 
 
