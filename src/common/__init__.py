f5eff9b7c0401462d3720df70e649160e6bcd358
vanand-dev <vanand@caltech.edu>
2017-02-03 16:48:20 -0800

    init merge

--- src/common/__init__.py
+++ src/common/__init__.py
@@ -28,6 +28,7 @@ def import_test(filename):
     with open(filename,'r') as dest_f:
         data_iter = csv.reader(dest_f, delimiter=',')
         data = [data for data in data_iter]
+    # remove first row (column labels), first column (id) and last column
     data_array = np.asarray(data)[1:, 1:]
     X_train = data_array.astype(np.float32)
     return X_train
