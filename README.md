acbe8c4932e615ee73354a94f5126c52afcbce60
Nikhil Gupta <nikhil97gupta@gmail.com>
2017-02-03 18:01:43 -0800

    Neural Net is very bad right now

--- README.md
+++ README.md
@@ -2,5 +2,8 @@
 
 This is a project for CS155 Machine Learning and Data Mining at Caltech.
 
+Neural Net:  
+    Currently always converging to the same value regardless of number of units
+
 TODO:
     Convert some columns to classes as opposed to integers
--- src/neural_net.py
+++ src/neural_net.py
@@ -17,21 +17,21 @@ def net(X, Y):
     # model.add(Activation('relu'))
     # model.add(Dropout(0.1))
 
-    model.add(Dense(500, input_dim=381))
-    model.add(Activation('relu'))
-    model.add(Dropout(0.1))
+    model.add(Dense(1, input_dim=381))
+    model.add(Activation('tanh'))
+    # model.add(Dropout(0.5))
 
-    model.add(Dense(250))
-    model.add(Activation('relu'))
+    # model.add(Dense(25))
+    # model.add(Activation('tanh'))
 
     model.add(Dense(1))
     model.add(Activation('softmax'))
 
     model.summary()
 
-    model.compile(loss='mse',optimizer='rmsprop', metrics=['accuracy'])
+    model.compile(loss='mse',optimizer='adagrad', metrics=['accuracy'])
 
-    fit = model.fit(X, Y, batch_size=256, nb_epoch=10, verbose=1)
+    fit = model.fit(X, Y, batch_size=128, nb_epoch=5, verbose=1)
 
     return model
 
@@ -53,8 +53,12 @@ def eval(model, X, Y):
 X_train_2008, Y_train_2008 = train_2008()
 model = net(X_train_2008, Y_train_2008)
 save(model, 'nn-500-250')
-# model = load('nn-750-500-250')
-# eval(model, X_train_2008, Y_train_2008)
+ 
+# X_test, ids = test_2008() 
+# model = load('nn-500-250')
+# print(model.get_weights())
+# predictions = model.predict(X_test)
+# print(sum(predictions))
 
 # to try and prevent an exception
 import gc; gc.collect()
