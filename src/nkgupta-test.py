ae5599ec2859b89958418cfa90fb2611c813efe7
Nikhil Gupta <nikhil97gupta@gmail.com>
2017-02-03 20:35:38 -0800

    partial analysis of labels

--- src/nkgupta-test.py
+++ src/nkgupta-test.py
@@ -6,5 +6,4 @@ from sklearn import svm
 X_train_2008, Y_train_2008 = train_2008()
 clf = svm.SVC()
 clf.fit(X_train_2008, Y_train_2008)
-joblib.dump(clf, 'saved_models/svc-default.jl')
 print(clf.score(X_train_2008, Y_train_2008))
