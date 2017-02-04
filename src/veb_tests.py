c5b6d011c911df3dacd3c7db513491ec76c6d512
vanand-dev <vanand@caltech.edu>
2017-02-03 16:53:23 -0800

    write todo

--- src/veb_tests.py
+++ src/veb_tests.py
@@ -1,1 +1,12 @@
-print('hello world')
+import numpy as np
+import time
+from common import *
+#import keras
+#import sklearn
+
+#print(train_data.shape, train_data.dtype)
+
+x, y = import_train('../data/train_2008.csv')
+x = import_test('../data/test_2008.csv')
+
+# veb will start out with some svm, linear regression, clustering, and stuff
