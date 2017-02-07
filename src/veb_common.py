5b1f1305c545312911c687b64ef92adb0070bdb1
vanand-dev <vanand@caltech.edu>
2017-02-07 09:36:59 -0800

    nn framework

--- src/veb_common.py
+++ src/veb_common.py
@@ -1,10 +1,7 @@
 #!/usr/bin/env python3
 
 import numpy as np
-import time
 from common import *
-from sklearn.model_selection import cross_val_score
-from sklearn.svm import SVC
 from sklearn.externals import joblib
 
 def normalized_data():
@@ -15,24 +12,42 @@ def normalized_data():
     std[std == 0] = 1  # in case values are constant for entire column
     X_train = (X_train - mean) / std
     return X_train, Y_train
-    
-def save_prediction(name, clf, year=2008):
-    X_train, Y_train = train_2008()
+
+def normalized_categorized_data():
+    X_train, Y_train = train_2008_categorized()
+    X_train, Y_train = X_train.astype(np.float32), Y_train.astype(np.float32)
+    #for arr in [X_train, Y_train]:
+    #   print arr.shape, arr.dtype
+    mean, std = np.mean(X_train, axis=0), np.std(X_train, axis=0)
+    std[std == 0] = 1  # in case values are constant for entire column
+    X_train = (X_train - mean) / std
+    return X_train, Y_train
+
+def save_prediction(name, clf, year, categorized):
+    if categorized:
+        X_train, Y_train = train_2008_categorized()
+    else:
+        X_train, Y_train = train_2008()
     mean, std = np.mean(X_train, axis=0), np.std(X_train, axis=0)
     std[std == 0] = 1
     print('saving', name)
     if year == 2008:
-        X_test, id_test = test_2008()
+        if categorized:
+            X_test, id_test = test_2008_categorized()
+        else:
+            X_test, id_test = test_2008()
     elif year == 2012:
-        X_test, id_test = test_2012()
+        if categorized:
+            X_test, id_test = test_2012_categorized()
+        else:
+            X_test, id_test = test_2012()
     X_test = (X_test - mean) / std
-    result = format_results(id_test, clf.predict(X_test))
+    result = format_results(id_test, clf.predict(X_test).astype(np.uint8))
     with open('submissions/' + str(year) + '-' + name + '.csv', 'w') as f:
         f.write(result)
 
-def clf_to_prediction(year, name):
-    save_prediction(name, joblib.load('saved_models/' + name), year)
-
-#clf_to_prediction(2008, 'd-MLP-(100, 50, 10)-0.02-0.790294895387-6')
+def clf_to_prediction(year, name, categorized=True):
+    save_prediction(name, joblib.load('saved_models/' + name), year, categorized)
 
 
+#clf_to_prediction(2008, 'b-SGD_hinge-1000-0.001-0.799078355266-533')
--- src/veb_neural_net.py
+++ src/veb_neural_net.py
@@ -8,23 +8,22 @@ from sklearn.externals import joblib
 from veb_common import *
 from sklearn.neural_network import MLPClassifier
 
-X_train, Y_train = normalized_data()
+X_train, Y_train = normalized_categorized_data()
 
-in_sample_accuracy = lambda clf: np.mean(clf.predict(X_train) == Y_train)
 
-def SVM(layers, tol=0.0001, cv=True):
+def NNC(layers, tol=0.0001, max_iter=100000, cv=True):
     '''
     param: cv: if cv is false, does not cross validate, trains on full data, 
     and pickles the result.
     param: frac_train: fraction of training to use for training (if False, 
     use all of the training data)
     '''
-    print('learning', layers, tol, cv)
+    print('learning', layers, tol, max_iter, cv)
     start = time.time()
     X, Y = X_train, Y_train
-    clf = MLPClassifier(hidden_layer_sizes=layers, verbose=True, tol=tol)
+    clf = MLPClassifier(hidden_layer_sizes=layers, verbose=True, tol=tol, max_iter=max_iter)
     if cv:
-        scores = cross_val_score(clf, X, Y, cv=3, 
+        scores = cross_val_score(clf, X, Y, cv=4, 
             scoring='accuracy', verbose=True)
         print('cross-validation accuracy:', np.mean(scores))
     else:
@@ -33,15 +32,15 @@ def SVM(layers, tol=0.0001, cv=True):
         score = clf.score(X_train, Y_train)
         print score
         # print np.mean(clf.predict(X_train)), np.mean(Y_train)
-        save_name = 'd-MLP-' + str(layers) + '-' + str(tol)
+        save_name = 'd-MLP-' + str(layers) + '-' + str(max_iter)
         save_name += '-' + str(score) + '-' + str(int(time.time() - start))
         joblib.dump(clf, 'saved_models/' + save_name)
     print('finished in', (time.time() - start), 'seconds')
 
 
 
-SVM((100, 50, 10), tol=.02, cv=False)
+NNC((500, 150), tol=.00001, max_iter=2, cv=True)
 
-#clf_to_prediction(2008, 'a-SVM_linear-0.762351740455-0.1-124')
+#clf_to_prediction(2008, 'd-MLP-200-1-0.79777939289-47')
 
 
