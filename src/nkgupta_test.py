1e7db2de3de48feece0a97207cb782f7f3de5fff
Nikhil Gupta <nikhil97gupta@gmail.com>
2017-02-06 16:47:26 -0800

    working version of to category, not finished

--- src/nkgupta_test.py
+++ src/nkgupta_test.py
@@ -2,14 +2,24 @@
 
 from common import *
 from sklearn import svm
-from keras.utils.np_utils import to_categorical
 
+def to_categorical(lst):
+    categories =  np.unique(lst, return_inverse=True)
+    max_val = np.max(categories) + 1
+    to_ret = np.zeros((max_val, len(lst)))
+
+    for i in range(len(categories)):
+        to_ret[categories[i]][i] = 1
+
+    return to_ret
 
 X_train_2008, Y_train_2008 = train_2008()
 test_2008, ids_2008 = test_2008()
 test_2012, ids_2012 = test_2012()
 
 descriptions = []
+# VERY MAGIC NUMBER
+NUM_DIM = 4056
 
 
 with open('../label_descriptions.txt','r') as dest_f:
@@ -29,24 +39,33 @@ X_train_2008 = X_train_2008.T
 
 assert len(X_train_2008) == len(descriptions)
 
+count = 0
+X_train_2008_fixed = np.empty( (NUM_DIM,len(X_train_2008[0])) )
+
 for i in range(len(X_train_2008)):
+    # print(i)
     # types of descriptions:
     # category, binary, binary/refused
     # integer
     label_type = descriptions[i]
-    if label_type == 'integer':
+    if label_type == 'delete':
+        pass
+    elif label_type == 'integer':
         # normalize to [0,1]
         min_val = np.min(X_train_2008[i])
         max_val = np.max(X_train_2008[i])
-        X_train_2008[i] = (X_train_2008[i] - min_val) / (max_val - min_val)
+
+        X_train_2008_fixed[count] = (X_train_2008[i] - min_val) / (max_val - min_val)
+        count += 1
     else:
         # category, binary, binary/refused
         # not finished, need to get around fixed size of numpy arrays
         # also implement to_categorical
-        print(X_train_2008[i])
-        print(to_categorical(X_train_2008[i]))
-        print(X_train_2008[i])
-        quit()
+        categorized = to_categorical(X_train_2008[i])
+        for j in range(len(categorized)):
+            X_train_2008_fixed[count] = categorized[j]
+            count += 1
 
-X_train_2008[0] = 1
-print(len(X_train_2008[0]))
+# print(X_train_2008_fixed.shape)
+print(count)
+assert count == NUM_DIM
